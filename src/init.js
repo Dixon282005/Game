773d0c73c124b100b0619d6a8b95ebadf363f1d4
Miguel <miguelxponce20@gmail.com>
2025-05-17 20:10:05 -0400

    Personaje del juego creado

--- src/init.js
+++ src/init.js
@@ -1,6 +1,6 @@
 const config = {
-    width: 300,
-    height: 180,
+    width: 320 * 2,
+    height: 180 * 2,
     parent: 'container',
     type: Phaser.AUTO,
     scene: {
@@ -8,18 +8,27 @@ const config = {
         create: create,
         update: update
     }
-}
+    // physics: {
+    //     default: 'arcade',
+    //     arcade: {
+    //         gravity: { y: 500 },
+    //     }
+    // }
+};
 
 var game = new Phaser.Game(config);
 
 function preload() {
-    console.log('Preload');
+    this.load.image('submarine', 'src/assets/submar.png'); // Ruta relativa
 }
 
 function create() {
-    console.log('Create');
+    // Crea el submarino CON física
+    this.submarine = this.add.image(80, 100, 'submarine');
+    this.submarine.setScale(1)
+        .setOrigin(0.5, 0.5)
+        // .setFlipX(true)
 }
 
-function update(time, delta) {
-    //console.log(delta);
+function update() {
 }
