0c1781069348014f0d155a0b55905264c0b160bb
Miguel <miguelxponce20@gmail.com>
2025-05-24 21:40:55 -0400

    Menu de inicio para comenzar juego

---
+++ src/mainMenu.js
@@ -0,0 +1,39 @@
+export default class MainMenu extends Phaser.Scene {
+    constructor() {
+        super('MainMenu');
+    }
+
+    preload() {
+        this.load.image('background', 'src/assets/drawed_bg.png');
+    }
+
+    create() {
+        // Fondo ajustado al tamaño de la pantalla
+        this.add.image(0, 0, 'background')
+            .setOrigin(0, 0)
+            .setDisplaySize(this.sys.game.config.width, this.sys.game.config.height);
+
+        // Título del juego
+        this.add.text(this.sys.game.config.width / 2, 80, 'SeaQuest 2.0', {
+            fontSize: '42px',
+            fill: '#ffffff',
+            fontFamily: 'Arial'
+        }).setOrigin(0.5);
+
+        // Texto para iniciar
+        this.add.text(this.sys.game.config.width / 2, 150, 'Presiona ENTER para comenzar', {
+            fontSize: '25px',
+            fill: '#ffffff',
+            fontFamily: 'Arial'
+        }).setOrigin(0.5);
+
+        // Captura de la tecla ENTER
+        this.enterKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+    }
+
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(this.enterKey)) {
+            this.scene.start('GameScene');
+        }
+    }
+}
