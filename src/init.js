547826bd1a076851cdb05eeb0c934e47f8f9be55
Miguel <miguelxponce20@gmail.com>
2025-05-27 15:41:11 -0400

    Debugger eliminado

--- src/init.js
+++ src/init.js
@@ -11,7 +11,7 @@ const config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 0 },
-
+            debug: false
         }
     },
     scene: [MainMenu, GameScene, PauseMenu], // Usa las escenas como un array
